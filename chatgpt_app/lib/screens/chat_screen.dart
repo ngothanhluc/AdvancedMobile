77ceb8c0d39e5bb77248743c4ce4a51b14043633
Thanh Luc <20120325@student.hcmus.edu.vn>
2023-10-13 14:04:51 +0700

    Create chat screen

---
+++ chatgpt_app/lib/screens/chat_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        leading: const Padding(padding: EdgeInsets.all(8)),
+        title: const Text("Chat GPT"),
+      ),
+    );
+  }
+}
